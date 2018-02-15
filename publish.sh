91d75635d4ea3ecd87f245344098599e0480f4c1
Fredrik Löwenhamn <lowet84@gmail.com>
2018-02-15 16:43:04 +0100

    Drone

--- publish.sh
+++ publish.sh
@@ -6,9 +6,10 @@ VERSION=$(cat GraphQlRethinkDbCore.csproj | grep PackageVersion | sed -n -r -e '
 NUGET_VERSION=$(curl https://api-v2v3search-0.nuget.org/query?q=GraphQlRethinkDbCore | jq '.data[0].versions[-1].version' --raw-output)
 echo $VERSION
 echo $NUGET_VERSION
-if [ 1 = 1 ]
+if [ $VERSION = $NUGET_VERSION ]
 then
   echo "Same version, exiting build"
   exit 0
 fi
 echo New version detected. Building and publishing
+
