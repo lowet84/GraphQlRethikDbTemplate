c167dccd57df6b94daea329769569c5ff0351628
root <root@Ubuntu.asdu3tqgoxnuzcsg1dc3czbidb.fx.internal.cloudapp.net>
2018-02-15 15:45:50 +0000

    Don’t mess with Voodoo

--- publish.sh
+++ publish.sh
@@ -6,10 +6,13 @@ VERSION=$(cat GraphQlRethinkDbCore.csproj | grep PackageVersion | sed -n -r -e '
 NUGET_VERSION=$(curl https://api-v2v3search-0.nuget.org/query?q=GraphQlRethinkDbCore | jq '.data[0].versions[-1].version' --raw-output)
 echo $VERSION
 echo $NUGET_VERSION
-if [ $VERSION = $NUGET_VERSION ]
-then
-  echo "Same version, exiting build"
-  exit 0
-fi
+#if [ $VERSION = $NUGET_VERSION ]
+#then
+#  echo "Same version, exiting build"
+#  exit 0
+#fi
 echo New version detected. Building and publishing
 
+dotnet restore
+dotnet pack
+find . -name *.nupkg
