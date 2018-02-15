9ed190ed8dc3b9c728f7dadcdc2321c6e2f832a6
Fredrik Löwenhamn <lowet84@gmail.com>
2018-02-15 16:55:16 +0100

    Drone

--- publish.sh
+++ publish.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
 cd $1
-apt-get update && apt-get install -y jq
 VERSION=$(cat GraphQlRethinkDbCore.csproj | grep PackageVersion | sed -n -r -e 's/.*?>(.*?)<.*/\1/p')
 NUGET_VERSION=$(curl https://api-v2v3search-0.nuget.org/query?q=GraphQlRethinkDbCore | jq '.data[0].versions[-1].version' --raw-output)
 echo $VERSION
 echo $NUGET_VERSION
-#if [ $VERSION = $NUGET_VERSION ]
-#then
-#  echo "Same version, exiting build"
-#  exit 0
-#fi
+if [ $VERSION = $NUGET_VERSION ]
+then
+ echo "Same version, exiting build"
+ exit 0
+fi
 echo New version detected. Building and publishing
 
 dotnet restore
 dotnet pack
-find . -name *.nupkg
+FILE=$(find . -name *.nupkg)
+nuget push $FILE -Source https://api.nuget.org/v3/index.json
