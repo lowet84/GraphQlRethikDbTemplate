3b466f8448a7a469cc45470688dfc4f9e38980f3
Fredrik Löwenhamn <lowet84@gmail.com>
2018-02-16 09:01:04 +0100

    Drone

--- publish.sh
+++ publish.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 cd $1
-VERSION=$(cat GraphQlRethinkDbCore.csproj | grep PackageVersion | sed -n -r -e 's/.*?>(.*?)<.*/\1/p')
-NUGET_VERSION=$(curl https://api-v2v3search-0.nuget.org/query?q=GraphQlRethinkDbCore | jq '.data[0].versions[-1].version' --raw-output)
+VERSION=$(cat $1.csproj | grep PackageVersion | sed -n -r -e 's/.*?>(.*?)<.*/\1/p')
+NUGET_VERSION=$(curl https://api-v2v3search-0.nuget.org/query?q=$1 | jq '.data[0].versions[-1].version' --raw-output)
 echo $VERSION
 echo $NUGET_VERSION
 if [ $VERSION = $NUGET_VERSION ]
@@ -15,4 +15,4 @@ echo New version detected. Building and publishing
 dotnet restore
 dotnet pack
 FILE=$(find . -name *.nupkg)
-nuget push $FILE -Source https://api.nuget.org/v3/index.json
+# nuget push $FILE -Source https://api.nuget.org/v3/index.json
