c32e6ed2f9908edea1553a45b8ae8c59a6ed796b
Fredrik Löwenhamn <lowet84@gmail.com>
2018-02-16 09:06:13 +0100

    Updated pusblish command

--- publish.sh
+++ publish.sh
@@ -15,4 +15,5 @@ echo New version detected. Building and publishing
 dotnet restore
 dotnet pack
 FILE=$(find . -name *.nupkg)
+nuget setApiKey $2
 nuget push $FILE -Source https://api.nuget.org/v3/index.json
