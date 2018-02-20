83db9365198c3f888a9153cfcf0186845db64161
root <root@Ubuntu.asdu3tqgoxnuzcsg1dc3czbidb.fx.internal.cloudapp.net>
2018-02-20 10:00:46 +0000

    Shaun broke the regex, lame

--- GraphQlRethinkDbHttp/SimpleHttpServer.cs
+++ GraphQlRethinkDbHttp/SimpleHttpServer.cs
@@ -128,7 +128,7 @@ namespace GraphQlRethinkDbHttp
         }
 
 
-        public Dictionary<string, string> MimeTypeMappings = new Dictionary<string, string>(StringComparer.InvariantCultureIgnoreCase) {
+        public Dictionary<string, string> MimeTypeMappings = new Dictionary<string, string>{
         {".asf", "video/x-ms-asf"},
         {".asx", "video/x-ms-asf"},
         {".avi", "video/x-msvideo"},
--- test.sh
+++ test.sh
@@ -2,4 +2,6 @@
 docker build -t gqltest .
 docker rm -f gqltest
 docker run -d -it --name gqltest --net=homeserver2 -e DATABASE=rethinkdb gqltest
-docker exec -it gqltest sh
+sleep 3
+docker exec -it gqltest curl http://localhost:3000 -v
+docker logs gqltest
